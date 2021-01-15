335dcc117a465c01367adda91db4209bdaa9c3fe
hxd <X.HanXD@Gmail.com>
2021-01-15 20:29:02 +0800

    Update diy-part2-mifi.sh

--- diy-part2-mifi.sh
+++ diy-part2-mifi.sh
@@ -19,10 +19,17 @@ sed -i '/^EOF/i \            \set wireless.default_radio${devidx}.key=1234567890
 # sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF./$1$GzaZpWin$e5M.CJ2ooGgDXrbQWltbd0/g' openwrt/package/lean/default-settings/files/zzz-default-settings
 # sed -i '/^exit 0/i echo 0xDEADBEEF > /etc/config/google_fu_mode' openwrt/package/lean/default-settings/files/zzz-default-settings
 
-sed -i 's/7552k/15872k/g' target/linux/ramips/image/mt7620.mk
-sed -i 's/760000/f80000/g' target/linux/ramips/dts/mt7620n_zbtlink_zbt-cpe102.dts
+# sed -i 's/7552k/15872k/g' target/linux/ramips/image/mt7620.mk
+# sed -i 's/760000/f80000/g' target/linux/ramips/dts/mt7620n_zbtlink_zbt-cpe102.dts
 # sed -i 's/7552k/15872k/g ; s/mt7620n/mt7620a/g' target/linux/ramips/image/mt7620.mk
 # sed -i '/zbtlink_zbt-cpe102/{s/7552k))$/15872k))/;}' target/linux/ramips/image/mt7620.mk
+
+#echo '删除旧版mt7620.mk,链接新版'
+rm -rf ./target/linux/ramips/image/mt7620.mk
+ln -s ../../../Dir/mt7620.mk ./target/linux/ramips/image
+
+rm -rf ./target/linux/ramips/dts/mt7620n_zbtlink_zbt-cpe102.dts
+ln -s ../../../Dir/mt7620n_zbtlink_zbt-cpe102.dts ./target/linux/ramips/dts
 
 #echo 'quectel'
 ln -s ../../../quectel ./package/lean/
