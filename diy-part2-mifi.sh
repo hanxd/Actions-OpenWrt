7ea54531c688c7b1473b501c87f0dbfd32301398
hxd <X.HanXD@Gmail.com>
2021-01-23 00:35:49 +0800

    Update diy-part2-mifi.sh

--- diy-part2-mifi.sh
+++ diy-part2-mifi.sh
@@ -53,3 +53,4 @@ git clone https://github.com/tty228/luci-app-serverchan.git package/mine/luci-ap
 # cp ../patches/997-hxd-ec20.patch target/linux/ramips/patches-5.4/997-hxd-ec20.patch
 # mv ../../../997-hxd-ec20.patch target/linux/ramips/patches-5.4/997-hxd-ec20.patch
 # mv ../../patches/997-hxd-ec20.patch target/linux/ramips/patches-5.4
+cp patches/997-hxd-ec20.patch target/linux/ramips/patches-5.4/997-hxd-ec20.patch
