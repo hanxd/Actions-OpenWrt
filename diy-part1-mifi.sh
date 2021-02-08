b0375ae15298d2d978a640970a80dc2e57b3d2ee
hxd <X.HanXD@Gmail.com>
2021-02-08 19:56:05 +0800

    Update diy-part1-mifi.sh

--- diy-part1-mifi.sh
+++ diy-part1-mifi.sh
@@ -15,3 +15,5 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 # sed -i '$a src-git vpasswall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
+
+sed -i '$a src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' feeds.conf.default
