73b14613b91a7158ddc60721dda14fdb2d858fa6
hxd <X.HanXD@Gmail.com>
2021-02-25 21:58:09 +0800

    Update diy-part2-op.sh

--- diy-part2-op.sh
+++ diy-part2-op.sh
@@ -26,20 +26,11 @@ sed -i '/^EOF/i \            \set wireless.default_radio${devidx}.key=567890321'
 sed -i 's/HNET C108/Mifi/g' target/linux/ramips/dts/mt7620a_hnet_c108.dts
 sed -i 's/wwan0/usb0/g' target/linux/ramips/mt7620/base-files/etc/board.d/01_leds
 
-# sed -i 's/760000/f80000/g' target/linux/ramips/dts/mt7620n_zbtlink_zbt-cpe102.dts
-# sed -i '/zbtlink_zbt-cpe102/{s/7552k))$/15872k))/;}' target/linux/ramips/image/mt7620.mk
-
-# echo '集成diy目录'
-# ln -s ../../diy ./package/openwrt-packages
-
 #=================================================
 # 主题DIY并拉取最新版
-pushd package/utils
-rm -rf luci-theme-argon
-# svn co https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom/trunk/luci-theme-infinityfreedom
-# rm -rf luci-theme-infinityfreedom/luci-theme-infinityfreedom/files/htdocs/images/main_bg_5k.jpg
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config.git luci-app-argon-config
+cd openwrt/package
+git clone https://github.com/jerrykuku/luci-theme-argon.git
+git clone https://github.com/jerrykuku/luci-app-argon-config.git
 popd
 #=================================================
 
@@ -51,34 +42,14 @@ wget https://github.com/hanxd/extd/releases/download/V1/997-hxd-ec20.patch
 popd
 #=================================================
 
-#=================================================
-# Travelmate
-# pushd feeds/packages/net
-# rm -rf travelmate
-# svn co https://github.com/openwrt/packages/trunk/net/travelmate
-# popd
-
-# pushd feeds/luci/applications
-# rm -rf luci-app-travelmate
-# svn co https://github.com/openwrt/luci/trunk/applications/luci-app-travelmate
-# popd
-#=================================================
-
 #echo 'Netgearrainbow'
 # ln -s ../../../luci-theme-netgearrainbow ./package/lean/
-
-# echo 'hotspot'
-# cp -f ../luci-app-hotspot package/lean/
-# git clone https://github.com/hanxd/extd.git package/mine/luci-app-hotspot
-
-#echo 'quectel'
-# ln -s ../../../quectel ./package/lean/
 
 # Server酱
 git clone https://github.com/tty228/luci-app-serverchan.git package/mine/luci-app-serverchan
 
 # luci-app-modeminfo
 # git clone https://github.com/koshev-msk/luci-app-atinout.git package/mine/luci-app-atinout
-git clone https://github.com/koshev-msk/luci-app-mmconfig.git package/mine/luci-app-mmconfig
+# git clone https://github.com/koshev-msk/luci-app-mmconfig.git package/mine/luci-app-mmconfig
 git clone https://github.com/koshev-msk/luci-app-modeminfo.git package/mine/luci-app-modeminfo
 git clone https://github.com/koshev-msk/luci-app-smstools3.git package/mine/luci-app-smstools3
